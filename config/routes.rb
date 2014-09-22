88d9ea8d090bdc2b988b31660c674b5e5a5f1923
Marc <m.bluemner@liquidlabs.de>
2014-09-22 15:38:49 +0200

    rail part sdone

--- config/routes.rb
+++ config/routes.rb
@@ -11,6 +11,11 @@ Rails.application.routes.draw do
     :controllers => {
       :omniauth_callbacks => "users/omniauth_callbacks" 
   }
+
+  devise_scope :user do
+    get '/api/current_user' => 'users/sessions#show_current_user', as: "show_current_user"
+    post '/api/check/is_user' => 'users/users#is_user', as: 'is_user'
+  end
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
