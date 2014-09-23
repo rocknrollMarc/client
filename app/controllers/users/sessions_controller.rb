5082c872a785fb38003c91b2baf74819c1fd1945
Marc <m.bluemner@liquidlabs.de>
2014-09-23 13:47:46 +0200

    updated sessionscontroller rails

--- app/controllers/users/sessions_controller.rb
+++ app/controllers/users/sessions_controller.rb
@@ -1,28 +1,32 @@
 class Users::SessionsController < Devise::SessionsController
-  repond_to :json
-  def show_current_user
-    reject_if_not_authorized_request!
+
+  skip_before_filter :verify_authenticity_token
+
+  before_filter :authenticate_user!, except: [:create]
+  respond_to :json
+
+  def create
+    resource = User.find_for_database_authentication(email: params[:user][:email])
+    return failure unless resource
+    return failure unless resource.valid_password?(params[:user][:password])
+
     render status: 200,
       json: {
         success: true,
-        info: "Current user",
-        user: current_user
+        info: "Logged in",
+        data: {
+          auth_token: current_user.authentication_token
+        }
     }
   end
 
   def failure
-    render status: 401,
+    warden.custom.failure!
+    render status: 200,
       json: {
         success: false,
-        info: "Unauthorized"
+        info: "Login failed",
+        data: {}
     }
-  end
-
-  private
-
-  def reject_if_not_authorized_request!
-    warden.authenticate!(
-      scope: resource_name,
-      recall: "#{controller_path}#failure")
   end
 end
