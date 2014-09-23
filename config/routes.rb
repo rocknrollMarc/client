5d16afde74eae2843428c15477368ae663b4afb6
Marc <m.bluemner@liquidlabs.de>
2014-09-23 13:50:35 +0200

    updated routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,22 +1,16 @@
 Rails.application.routes.draw do
-  
-  get '/dashboard' => 'welcome#dashboard'
-  root to: 'welcome#index'
+ scope '/api' do
+   resources :shares
 
-  namespace :api do
-    resources :shares
-  end
+   devise_for :users,
+     :controllers => {
+      omniauth_callbacks: "users/omniauth_callbacks",
+      registrations: "users/registrations",
+      sessions: "users/sessions"
+   }
+ end
 
-  scope '/api' do
-  devise_for :users,
-    :controllers => {
-      :omniauth_callbacks => "users/omniauth_callbacks",
-      registrations: "users/registrations"
-  }
-  end
+ get '/dashboard' => 'welcome#dashboard'
+ root to: 'welcome#index'
 
-  devise_scope :user do
-    get '/api/current_user' => 'users/sessions#show_current_user', as: "show_current_user"
-    post '/api/check/is_user' => 'users/users#is_user', as: 'is_user'
-  end
 end
