225c0dce2729deabb3120020941c87cb6671b3f0
Marc <m.bluemner@liquidlabs.de>
2014-09-23 13:28:31 +0200

    login controller angular

--- client/app/scripts/controllers/login.js
+++ client/app/scripts/controllers/login.js
@@ -1,2 +1,17 @@
 angular.module('shareupApp')
-.controller('LoginCtrl', function() {});
+.controller('LoginCtrl', function($location, $scope, $http) {
+  $scope.signup = function() {
+    $http({
+      url: '/api/users',
+      method: 'POST',
+      data: {
+        user: $scope.user
+      }
+    }).success(function(data) {
+      $scope.$broadcast('event:authenticated');
+      $location.path('/');
+    }).error(function(reason) {
+      $scope.user.errors = reason;
+    });
+  };
+});
