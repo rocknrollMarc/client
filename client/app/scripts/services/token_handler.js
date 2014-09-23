6377c9abff63bd63d689d559d9e9aecd08256ae4
Marc <m.bluemner@liquidlabs.de>
2014-09-23 13:36:03 +0200

    token handler service

---
+++ client/app/scripts/services/token_handler.js
@@ -0,0 +1,17 @@
+angular.module('shareupApp')
+.factory('tokenHandler', function($rootScope, $http, $q, $location) {
+  var token = null,
+  currentUser;
+
+  var tokenHandler = {
+    set: function(v) { token = v; },
+    get: function() {
+      if (!token) 
+        $rootScope.$broadcast('event:unauthorized');
+        else
+          return token;
+      }
+    };
+
+    return tokenHandler;
+});
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140922131058) do
+ActiveRecord::Schema.define(version: 20140922164258) do
 
   create_table "authorizations", force: true do |t|
     t.string   "provider"
@@ -48,8 +48,10 @@ ActiveRecord::Schema.define(version: 20140922131058) do
     t.string   "last_sign_in_ip"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.string   "authentication_token"
   end
 
+  add_index "users", ["authentication_token"], name: "index_users_on_authentication_token", unique: true
   add_index "users", ["email"], name: "index_users_on_email", unique: true
   add_index "users", ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
 
